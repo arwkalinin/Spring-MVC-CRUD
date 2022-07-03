8da13b7a4b8cd26f53dd16b14be546acef56d962
Alexander Kalinin <arw.kalinin@gmail.com>
2022-07-03 10:10:44 +0300

    PersonValidator

--- src/main/java/com/arwka/springapp/dao/PersonDAO.java
+++ src/main/java/com/arwka/springapp/dao/PersonDAO.java
@@ -11,6 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class PersonDAO {
@@ -37,7 +38,13 @@ public class PersonDAO {
                         new BeanPropertyRowMapper<>(Person.class))
                 .stream().findAny().orElse(null);
     }
-
+    public Optional<Person> show(String email) {
+        return jdbcTemplate.query(
+                        "SELECT * FROM Person WHERE email=?",
+                        new Object[]{email},
+                        new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny();
+    }
     //
     public void save(Person person) {
         jdbcTemplate.update(
---
+++ src/main/java/com/arwka/springapp/util/PersonValidator.java
@@ -0,0 +1,28 @@
+package com.arwka.springapp.util;
+
+import com.arwka.springapp.dao.PersonDAO;
+import com.arwka.springapp.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class PersonValidator implements Validator {
+
+    private final PersonDAO personDAO;
+    @Autowired
+    public PersonValidator(PersonDAO personDAO) { this.personDAO = personDAO; }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Person.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Person person = (Person) o;
+        if (personDAO.show(person.getEmail()).isPresent())
+            errors.rejectValue("email", "", "This email is already taken.");
+    }
+}
